d791b8c98d7ab4978a357731e27cba6d99d6f9fd
Josue23 <josuerodrigues@gmail.com>
2017-02-08 07:29:23 -0200

    execício simulador-dados-03.py

---
+++ simulador-dados-03.py
@@ -0,0 +1,19 @@
+import random
+continua = True
+
+# connstrói a função que sorteia as faces de cada dado
+def sorteia_numeros(faces):
+    dados = int(input('Número de dados: '))
+    for dado in range(dados): # range gera uma LISTA de 0 até número de dados menos 1
+        numero = random.randint(1, faces)
+        print('%dº dado: %d ' %(dado+1, numero))
+
+while continua:
+    faces = input('\nPressione a tecla ESPAÇO para parar ou \nDigite um número de faces: ')
+    #if faces == "parar":
+    if faces == chr(32): # tecla space
+        continua = False # the loop stops
+    else:
+        faces = int(faces)
+        sorteia_numeros(faces)  #  chama a função sorteia_numeros
+print("Fim do programa!!")
